f944de4f1390cd56de3706f0e8cc5983252f20c0
liimonx <limonkhan669@gmail.com>
2018-05-31 12:36:35 +0600

    wevpack 4 boilerplate

---
+++ app/index.js
@@ -0,0 +1,15 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+const sum = function(a,b){
+    return a+b;
+}
+let result = sum(52,20);
+console.log(result);
